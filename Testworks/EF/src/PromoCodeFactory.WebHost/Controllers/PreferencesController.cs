206ab700d24af294b0326513d658323c7ae411eb
DrZoidberg10 <145379244+DrZoidberg10@users.noreply.github.com>
2025-03-18 17:52:23 +0300

    task 6

---
+++ Testworks/EF/src/PromoCodeFactory.WebHost/Controllers/PreferencesController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PromoCodeFactory.Core.Abstractions.Repositories;
+using PromoCodeFactory.Core.Domain.PromoCodeManagement;
+using PromoCodeFactory.WebHost.Models;
+
+namespace PromoCodeFactory.WebHost.Controllers
+{
+    /// <summary>
+    /// Предпочтения
+    /// </summary>
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class PreferencesController
+        : ControllerBase
+    {
+        private readonly IRepository<Preference> _preferenceRepository;
+
+        public PreferencesController(IRepository<Preference> preferenceRepository)
+        {
+            _preferenceRepository = preferenceRepository;
+        }
+
+        /// <summary>
+        /// Получить список предпочтений
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<List<PreferenceResponse>> GetPreferencesAsync()
+        {
+            var preferences = await _preferenceRepository.GetAllAsync();
+
+            var preferencesModelList = preferences.
+                Select(
+                    p => new PreferenceResponse()
+                    {
+                        Id = p.Id,
+                        Name = p.Name
+                    })
+                .ToList();
+
+            return preferencesModelList;
+        }
+    }
+}
